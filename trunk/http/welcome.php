c4df861720a32fa86709d2d9d2fc2cff35ce1aac
Collin Bourdage <collin.bourdage@gmail.com>
2012-05-06 12:41:34 -0500

    made updates to css and js

---
+++ trunk/http/welcome.php
@@ -0,0 +1,80 @@
+<!--[if lt IE 7]><html class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class=""><!--<![endif]-->
+<head>
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+	<meta http-equiv="Content-Language" content="en-US" >
+	<title>Every Last Morsel</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300|Junge' rel='stylesheet' type='text/css'>
+	<link href="/file-bin/css/960/reset.css" media="screen" rel="stylesheet" type="text/css" >
+	<link href="/file-bin/css/960/960.css" media="screen" rel="stylesheet" type="text/css" >
+	<link href="/file-bin/css/welcome.css" media="screen" rel="stylesheet" type="text/css" >
+	<link rel="shortcut icon" href="/favicon.ico">
+
+	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+
+<body id="page">
+<div class="container">
+	<header class="clearfix">
+		<h1><a href="#" title="Every Last Morsel">Every Last Morsel</a></h1>
+		<ul role="navigation">
+			<li><a href="#how-it-works" title="Hot it works">How It Works</a></li>
+			<li><a href="http://www.facebook.com/everylastmorsel" target="_blank" title="Facebook">Facebook</a></li>
+			<li><a href="http://www.twitter.com/@everylastmorsel" target="_blank" title="Twitter">Twitter</a></li>
+		</ul>
+	</header>
+</div>
+<div class="container hero">
+	<div id="hero" class="clearfix">
+		<h3>mary, mary, quite contrary where does your garden grow?</h3>
+
+		<form role="newsletter" class="group" action="" method="post">
+			<ul>
+				<li>
+					<label for="email">Email:</label>
+					<div><input type="email" name="email" id="email" value="<?= $email; ?>" /></div>
+				</li>
+				<li>
+					<label for="email">Region:</label>
+					<div><input type="text" name="region" id="region" value="<?= $region; ?>" placeholder="(Chicago, Boston, LA, etc.)" /></div>
+				</li>
+			</ul>
+			<p><input type="submit" name="submit" value="Sign Up" /></p>
+		</form>
+	</div>
+</div>
+<div class="container">
+	<div>
+
+	</div>
+</div>
+<div class="container roots">
+	<div id="roots">
+		<ol>
+			<li>
+				<h3>Title</h3>
+				<p></p>
+			</li>
+			<li>
+				<h3>Title 2</h3>
+				<p></p>
+			</li>
+			<li>
+				<h3>Title 3</h3>
+				<p></p>
+			</li>
+			<li>
+				<h3>Title 4</h3>
+				<p></p>
+			</li>
+		</ol>
+	</div>
+	<footer>
+		<p class="copyright">Copyright 2012 Every Last Morsel</p>
+	</footer>
+</div>
+</body>
+</html>
