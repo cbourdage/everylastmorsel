14be15644b4659330a30483da58a067bcfef78b9
Collin Bourdage <collin.bourdage@gmail.com>
2012-07-29 21:01:21 -0500

    updates to community pages.

--- application/controllers/CommunityController.php
+++ application/controllers/CommunityController.php
@@ -8,20 +8,37 @@ require_once 'controllers/AbstractController.php';
 class Elm_CommunityController extends Elm_AbstractController
 {
 	/**
+	 * Default 404
+	 */
+	public function noRouteAction()
+	{
+	}
+
+	/**
 	 * @return void
 	 */
     public function indexAction()
     {
 		$model = Elm::getSingleton('community');
 		$this->view->users = $model->getUsers();
-		$this->view->plots = $model->getPlots();
+    }
+
+
+	/**
+	 * @return void
+	 */
+    public function peopleAction()
+    {
+		$this->_forward('index');
     }
 
 	/**
-	 * Default 404
+	 * @return void
 	 */
-	public function noRouteAction()
-	{
-	}
+    public function plotsAction()
+    {
+		$model = Elm::getSingleton('community');
+		$this->view->plots = $model->getPlots();
+    }
 }
 
